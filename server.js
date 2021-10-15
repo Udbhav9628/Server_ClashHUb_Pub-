fbe80f7a3509b28fb8c0c497623c56da33b08d0f
UDBHAV VIKRAM SINGH <udbhav9628@gmail.com>
2021-10-15 23:54:45 +0530

    Video  $(   completed

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const dotenv = require('dotenv');
+const app = express();
+const connectDB = require('./server/database/connection');
+
+dotenv.config({path:path.join(__dirname,'config.env')});
+
+connectDB();
+
+const port = process.env.PORT;
+
+app.use(morgan('tiny'));
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use('/',require('./server/routes/router'));
+
+app.listen(port,()=>{
+  console.log("The server is running at poort 8000");
+});

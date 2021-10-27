63fbe1b0c4e9352e81a01cbfd42b604be37941b0
UDBHAV VIKRAM SINGH <udbhav9628@gmail.com>
2021-10-27 22:59:02 +0530

    App Completed 72 comp

--- server/routes/User.js
+++ server/routes/User.js
@@ -3,7 +3,6 @@ const route = express.Router();
 const path = require("path");
 const dotenv = require("dotenv");
 const Userlogin = require("../model/Userlogin");
-// const Notes = require("../model/Notes");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
@@ -21,20 +20,20 @@ route.get("/", (req, res) => {
 route.post(
   "/createuser",
   [
-    body("Name", "Name can only be Letters").isAlpha(),
     body("Email").isEmail(),
     body("Password").isLength({ min: 5 }),
   ],
   async (req, res) => {
+    let sucess = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({errors: errors.array() });
     }
 
     try {
       let Newuser = await Userlogin.findOne({ Email: req.body.Email });
       if (Newuser) {
-        res.status(404).send("user already Existed");
+        res.status(404).send({sucess,Message:"user already Existed"});
         return;
       } else {
         //Creating User
@@ -47,18 +46,19 @@ route.post(
         });
         Newuser.save()
           .then(() => {
+            sucess=true;
             const Jwtdata = {
               id: Newuser.id,
             };
             const Auth_Token = jwt.sign(Jwtdata, Jwt_screat);
-            res.json({ Auth_Token });
+            res.json({sucess, Auth_Token });
           })
           .catch((error) => {
             res.send({ Message: error.message });
           });
       }
     } catch (error) {
-      res.status(500).send(error.message);
+      res.status(500).send({ Message: error.message });
     }
   }
 );
@@ -71,9 +71,10 @@ route.post(
     body("Password").isLength({ min: 5 }),
   ],
   async (req, res) => {
+    let sucess = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ sucess, errors: errors.array() });
     }
 
     try {
@@ -81,7 +82,7 @@ route.post(
       if (!user) {
         return res
           .status(500)
-          .json({ Message: "Login With Correct Credientals Please" });
+          .json({sucess, Message: "Login With Correct Credientals Please" });
       } else {
         const PassWord_comp = await bcrypt.compare(
           req.body.Password,
@@ -90,15 +91,16 @@ route.post(
         if (!PassWord_comp) {
           res
             .status(500)
-            .json({ Message: "Login With Correct Credientals Please" });
+            .json({ sucess, Message: "Login With Correct Credientals Please" });
           return;
         } else {
+          sucess=true;
           //Giving PayLoad To Jwt
           const PayLoad = {
             id: user.id,
           };
           const Auth_Token = jwt.sign(PayLoad, Jwt_screat);
-          res.json({ Auth_Token });
+          res.json({ sucess, Auth_Token });
         }
       }
     } catch (error) {
@@ -108,15 +110,15 @@ route.post(
 );
 
 //Post request Get Loggedin User Details | Login Required
-route.post("/GetuserDetails", fetchuser, async (req, res) => {
-  try {
-    const userid = req.user.id;
-    const user = await Userlogin.findById(userid).select();
-    res.send(user)
+// route.post("/GetuserDetails", fetchuser, async (req, res) => {
+//   try {
+//     const userid = req.user.id;
+//     const user = await Userlogin.findById(userid).select();
+//     res.send(user)
 
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
 
 module.exports = route;

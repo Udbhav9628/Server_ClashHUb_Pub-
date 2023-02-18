a16b127b2305143088d3f43ac83b6aa249ab190f
Udbhav9628 <udbhav9628@gmail.com>
2023-02-18 16:44:03 +0530

    Removing Secret from History

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const error = require("./server/Middleware/error");
 const bodyParser = require("body-parser");
 const http = require("http");
 
-//Handling uncaught Exception
-//***To Do **/ 1:21 min in video
-
 app.use(cors());
 app.use(error);
 
@@ -42,7 +39,6 @@ const server = http_server.listen(port, () => {
   console.log("The server is running at port 8000");
 });
 
-//Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log("Shutting down the server because of unhendled rejection error");
